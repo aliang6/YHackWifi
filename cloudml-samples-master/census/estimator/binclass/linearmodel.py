535c28954ba54133ae7744eff0dfb35267503fa7
Johnny So <jso123450@gmail.com>
2017-12-02 17:06:11 -0500

    pushed

--- cloudml-samples-master/census/estimator/binclass/linearmodel.py
+++ cloudml-samples-master/census/estimator/binclass/linearmodel.py
@@ -10,66 +10,83 @@ import numpy as np
 import tensorflow as tf
 
 # Data sets
-IRIS_TRAINING = "iris_training.csv"
-IRIS_TRAINING_URL = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + \
-    "&q=state:Alaska" + "&rows=100" + "&fl=name,DOB,sex,latitude,longitude"
+PARTICIPANT_TRAINING = "PARTICIPANT_TRAINING.csv"
+PARTICIPANT_TRAINING_URL = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + \
+    "&q=state:Alaska" + "&rows=1000" + "&fl=name,DOB,sex,latitude,longitude"
 
-IRIS_TEST = "iris_test.csv"
-IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
+PARTICIPANT_TEST = "PARTICIPANT_TEST.csv"
+PARTICIPANT_TEST_URL = PARTICIPANT_TRAINING_URL
 
-keys = ["latitude", "longitude"]
-heading = [0, (len(keys))] + keys
+keys = ["sex", "EMPLOYMENT_STATUS", "TOBACCO", "MARITAL_STATUS", "latitude", "longitude"]
+plan_ranks = ["BRONZE", "SILVER", "GOLD", "PLATINUM"]
+heading = [0, (len(keys))] + plan_ranks
+str_to_nums_dict = {"sex": {"male": 0, "female": 1},
+                    "EMPLOYMENT_STATUS": {"Unemployed": 0, "Employed": 1},
+                    "TOBACCO": {"NO": 0, "YES": 1},
+                    "MARITAL_STATUS": {"S": 0, "M": 1} }
+plan_to_nums_dict = {"BRONZE": 0,
+                     "SILVER": 1,
+                     "GOLD": 2,
+                     "PLATINUM": 3 }
+
 
 
 def main():
     # If the training and test sets aren't stored locally, download them.
-    if not os.path.exists(IRIS_TRAINING):
-        raw = urlopen(IRIS_TRAINING_URL).read()
+    if not os.path.exists(PARTICIPANT_TRAINING):
+        raw = urlopen(PARTICIPANT_TRAINING_URL).read()
         raw = json.loads(raw.decode('utf-8'))['response']['docs']
         raw = [{key: i[key] for key in keys} for i in raw]
         heading[0] = len(raw)
         print(heading)
-        with open(IRIS_TRAINING, "wb") as f:
+        with open(PARTICIPANT_TRAINING, "wb") as f:
             for h in heading:
                 f.write(b"" + str(h).encode() + b",")
             f.write(b'\n')
-            for i in raw:
-                f.write(b"" + str(i['latitude']**2).encode() + b"," + str(i['longitude']** 2).encode() + b"," + str(random.randint(0, 2)).encode() + b'\n')
+            for d in raw:
+                for key in d:
+                    if key in str_to_nums_dict:
+                        f.write(b"" + str( str_to_nums_dict[key][d[key]] ).encode() + b",")
+                    else:
+                        f.write(b"" + str(d[key]) + b",")
+            f.write(b"," + str(random.randint(0,3)) + b'\n');
 
-    if not os.path.exists(IRIS_TEST):
-        raw = urlopen(IRIS_TRAINING_URL).read()
+    if not os.path.exists(PARTICIPANT_TEST):
+        raw = urlopen(PARTICIPANT_TRAINING_URL).read()
         raw = json.loads(raw.decode('utf-8'))['response']['docs']
         raw = [{key: i[key] for key in keys} for i in raw]
         heading[0] = len(raw)
-        with open(IRIS_TEST, "wb") as f:
+        with open(PARTICIPANT_TEST, "wb") as f:
             for h in heading:
                 f.write(b"" + str(h).encode() + b",")
             f.write(b'\n')
-            for i in raw:
-                f.write(b"" + str(i['latitude']**2).encode() + b"," + str(i['longitude']** 2).encode() + b"," + str(random.randint(0, 2)).encode() + b'\n')
-                #raw = urlopen(IRIS_TEST_URL).read()
-                # with open(IRIS_TEST, "wb") as f:
-                # f.write(raw)
+            for d in raw:
+                for key in d:
+                    if key in str_to_nums_dict:
+                        f.write(b"" + str( str_to_nums_dict[key][d[key]] ).encode() + b",")
+                    else:
+                        f.write(b"" + str(d[key]) + b",")
+            f.write(b"," + str(random.randint(0,3)) + b'\n');
 
     # Load datasets.
     training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=IRIS_TRAINING,
+        filename=PARTICIPANT_TRAINING,
         target_dtype=np.int,
         features_dtype=np.float32)
     print(training_set)
     test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=IRIS_TEST,
+        filename=PARTICIPANT_TEST,
         target_dtype=np.int,
         features_dtype=np.float32)
 
     # Specify that all features have real-value data
-    feature_columns = [tf.feature_column.numeric_column("x", shape=[2])]
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[len(keys)])]
 
     # Build 3 layer DNN with 10, 20, 10 units respectively.
     classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
-                                            n_classes=3,
-                                            model_dir="/tmp/iris_model")
+                                            n_classes=len(plan_ranks),
+                                            model_dir="/tmp/insurance_plan_model")
     # Define the training inputs
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": np.array(training_set.data)},
