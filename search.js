a3cb0992a86951003e97ad05e764fe0b1315c436
Andy Liang <andy.liang.1@stonybrook.edu>
2017-12-02 19:18:00 -0500

    Front page

--- search.js
+++ search.js
@@ -12,23 +12,45 @@ var BASE_PRICE, AD_D, DEDUCTIBLE, PLAN_NAME, COVERAGE_AMOUNT;
 //JSON
 var json;
 
-function store() {
-	cust_name = getElementsByName("cust_name");
-	sex = getElementsByName("sex");
-	DOB = getElementsByName("DOB");
-	state = getElementsByName("state");
-	city = getElementsByName("city");
-	var json = search();
-	document.getElementsByName("json").innerHTML = json;
+function fadeOut(elements) {
+    var alpha = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (elements.style.opacity <= 0.1){
+            clearInterval(timer);
+            //elements.style.display = 'none';
+        }
+        elements.style.opacity = alpha;
+        alpha -= alpha * 0.1;
+    }, 50);
 }
 
-getElementsByName("Submit").onclick = 
-	function search() {
-		//Search Participants and retrieve the Participant name, date of birth and gender
-		//SELECT pi.name, pi.DOB, pi.sex FROM v_participant pi WHERE pi.state = 'Alaska'
-		url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + "&q=state:" + state + "+sex:" + sex "&rows=100" + "&fl=name,DOB,sex,state";
-		fetch(url)
-			.then(res => res.json())
-			.then(body => alert(body.response.numFound))
-			.catch(alert)
-	}
+function getName() {
+	cust_name = document.getElementById("cust_name").value;
+	fadeOut(document.getElementById("fade"));
+}
+
+document.getElementById("submit").addEventListener("click", getName());
+
+function store() {
+	sex = document.getElementById("sex").value;
+	DOB = document.getElementById("DOB").value;
+	state = document.getElementById("state").value;
+	city = document.getElementById("city").value;
+	var json = JSON.parse(search());
+	document.innerHTML = "Your mom"
+	document.innerHTML = json;
+	//document.getElementsById("json").innerHTML = json;
+}
+
+//document.getElementById("submit").addEventListener("click", function(){ alert("Your mom"); });
+
+
+function search() {
+	//Search Participants and retrieve the Participant name, date of birth and gender
+	//SELECT pi.name, pi.DOB, pi.sex FROM v_participant pi WHERE pi.state = 'Alaska'
+	url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + "&q=state:" + state + "+sex:" + sex + "&rows=100" + "&fl=name,DOB,sex,state";
+	fetch(url, {mode: 'cors'})
+		.then(res => res.json())
+		.then(body => alert(body.response.numFound))
+		.catch(alert)
+}
